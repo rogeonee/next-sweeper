29aa55d93e3745293d780bbf06bf4188ae5dbb8c
Egor <egormaxb@gmail.com>
2024-09-19 19:43:28 -0700

    feat: tutorial, basic game mechanics

---
+++ helpers/createCellMap.ts
@@ -0,0 +1,11 @@
+import { Cell } from '@/types/Cell';
+import { CellId } from '@/types/CellId';
+
+export function createCellMap(cells: Cell[]) {
+  const cellMap: Map<CellId, Cell> = new Map();
+  for (const cell of cells) {
+    cellMap.set(cell.id, cell);
+  }
+
+  return cellMap;
+}
