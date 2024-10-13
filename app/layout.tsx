6d198a813ad843f242a773934f1a07c4da28c322
Egor <egormaxb@gmail.com>
2024-10-12 22:29:13 -0700

    feat: style home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 
+import { Center } from '@/components/Center';
 import Link from 'next/link';
 import type { Metadata } from 'next';
 import { spaceMono } from './fonts';
@@ -16,18 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={` antialiased bg-black text-white`}>
+      <body className="antialiased bg-black text-white">
         <main className="flex flex-col items-center">
           <header className="m-8">
-            <hgroup>
-              <h1 className={spaceMono.className}>
-                <Link href="/">Minesweeper</Link>
-              </h1>
-            </hgroup>
+            <hgroup></hgroup>
           </header>
 
-          <div className="w-96">{children}</div>
+          <div className="max-w-96">{children}</div>
         </main>
+        <footer className="text-gray-500 text-xs mt-32 mb-16 mx-8">
+          <Center>Adapted by Egor</Center>
+        </footer>
       </body>
     </html>
   );
